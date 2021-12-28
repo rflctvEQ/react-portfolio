a576189e8d44182844d35386258dca6c71549c4b
Ian Everitt <ian.andrew.everitt@gmail.com>
2021-12-28 16:33:44 -0600

    updated projects and modified mobile view to be more responsive

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -32,33 +32,33 @@ function Projects() {
                         >
                             <figure className="col-10 col-md-4 col-lg-3 project project1">
                                 <div className="description">
-                                    <p className="project-title">Inspecto-Gadget</p>
-                                    <p>MVP of app inspectors and administrators in Orchard Lake, MI use for tracking inspections.</p>
-                                    <p className='username'>User: Electrical</p>
-                                    <p className='password'>Password: pass12345</p>
+                                    <p className="project-title">Enchanted Quill</p>
+                                    <p>React/TypeScript/Express/MySQL ecommerce site under construction for Enchanted Quill Artwork, LLC.</p>
                                     <div className="link-buttons">
-                                        <a href="https://github.com/Laurenzel93/inspecto-gadget" target="_blank" className="github-link">GitHub Repo</a>
-                                        <a href="https://arcane-taiga-84005.herokuapp.com/" target="_blank" className="project-link">Heroku</a>
+                                        <a href="https://github.com/rflctvEQ/laughing-broccoli" target="_blank" className="github-link">GitHub Repo</a>
+                                        <a href="https://calm-oasis-15641.herokuapp.com/" target="_blank" className="project-link">Visit Site</a>
                                     </div>
                                 </div>
                             </figure>
                             <figure className="col-10 col-md-4 col-lg-3 project project2">
                                 <div className="description">
-                                    <p className="project-title">Group Mentality</p>
-                                    <p>A full stack app where you can post to a public forum and have direct interactions with mental health specialists.</p>
+                                    <p className="project-title">This Game</p>
+                                    <p>React/TypeScript interactive story with subpar writing.</p>
                                     <div className="link-buttons">
-                                        <a href="https://github.com/rflctvEQ/group-mentality" target="_blank" className="github-link">GitHub Repo</a>
-                                        <a href="https://protected-waters-68869.herokuapp.com/" target="_blank" className="project-link">Heroku</a>
+                                        <a href="https://github.com/rflctvEQ/this-game" target="_blank" className="github-link">GitHub Repo</a>
+                                        <a href="https://stoic-kowalevski-619236.netlify.app/" target="_blank" className="project-link">Visit Site</a>
                                     </div>
                                 </div>
                             </figure>
                             <figure className="col-10 col-md-4 col-lg-3 project project3">
                                 <div className="description">
-                                    <p className="project-title">Tech Blog</p>
-                                    <p>A full stack app for techies to blog about their latest projects.</p>
+                                    <p className="project-title">Inspecto-Gadget</p>
+                                    <p>MVP of React/JS/Express/MySQL app building inspectors in Orchard Lake, MI use for tracking inspections.</p>
+                                    <p className='username'>User: Electrical</p>
+                                    <p className='password'>Password: pass12345</p>
                                     <div className="link-buttons">
-                                        <a href="https://github.com/rflctvEQ/tech-blog" target="_blank" className="github-link">GitHub Repo</a>
-                                        <a href="https://afternoon-retreat-69044.herokuapp.com/" target="_blank" className="project-link">Heroku</a>
+                                        <a href="https://github.com/Laurenzel93/inspecto-gadget" target="_blank" className="github-link">GitHub Repo</a>
+                                        <a href="https://arcane-taiga-84005.herokuapp.com/" target="_blank" className="project-link">Visit Site</a>
                                     </div>
                                 </div>
                             </figure>
@@ -75,31 +75,31 @@ function Projects() {
                         >
                             <figure className="col-10 col-md-4 col-lg-3 project project4">
                                 <div className="description">
-                                    <p className="project-title">Note Taker</p>
-                                    <p>Take, edit, and delete notes with a clean UI.</p>
+                                    <p className="project-title">Group Mentality</p>
+                                    <p>JS/Handlebars/Experss/MySQL app where you can post to a public forum and have direct interactions with mental health specialists.</p>
                                     <div className="link-buttons">
-                                        <a href="https://github.com/rflctvEQ/note-taker" target="_blank" className="github-link">GitHub Repo</a>
-                                        <a href="https://young-savannah-42064.herokuapp.com/" target="_blank" className="project-link">Heroku</a>
+                                        <a href="https://github.com/rflctvEQ/group-mentality" target="_blank" className="github-link">GitHub Repo</a>
+                                        <a href="https://protected-waters-68869.herokuapp.com/" target="_blank" className="project-link">Visit Site</a>
                                     </div>
                                 </div>
                             </figure>
                             <figure className="col-10 col-md-4 col-lg-3 project project5">
                                 <div className="description">
-                                    <p className="project-title">Employee Directory</p>
-                                    <p>React front end app that renders a searchable and sortable employee directory from the Random User API.</p>
+                                    <p className="project-title">Tech Blog</p>
+                                    <p>JS/Handlebars/Experss/MySQL app for techies to blog about their latest projects.</p>
                                     <div className="link-buttons">
-                                        <a href="https://github.com/rflctvEQ/employee-directory" target="_blank" className="github-link">GitHub Repo</a>
-                                        <a href="https://rflctveq.github.io/employee-directory" target="_blank" className="project-link">Visit site</a>
+                                        <a href="https://github.com/rflctvEQ/tech-blog" target="_blank" className="github-link">GitHub Repo</a>
+                                        <a href="https://afternoon-retreat-69044.herokuapp.com/" target="_blank" className="project-link">Visit Site</a>
                                     </div>
                                 </div>
                             </figure>
                             <figure className="col-10 col-md-4 col-lg-3 project project6">
                                 <div className="description">
-                                    <p className="project-title">Fitness Tracker</p>
-                                    <p>Record your workouts and see a visual, graphical representation of your progress!</p>
+                                    <p className="project-title">Employee Directory</p>
+                                    <p>React/JavaScript app that renders a searchable and sortable employee directory from the Random User API.</p>
                                     <div className="link-buttons">
-                                        <a href="https://github.com/rflctvEQ/workout-tracker" target="_blank" className="github-link">GitHub Repo</a>
-                                        <a href="https://stark-river-86497.herokuapp.com/" target="_blank" className="project-link">Heroku</a>
+                                        <a href="https://github.com/rflctvEQ/employee-directory" target="_blank" className="github-link">GitHub Repo</a>
+                                        <a href="https://rflctveq.github.io/employee-directory" target="_blank" className="project-link">Visit Site</a>
                                     </div>
                                 </div>
                             </figure>
