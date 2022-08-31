e0b4076146a8a9fffaa28cadc447d797cac874a2
Ian Everitt <ian.andrew.everitt@gmail.com>
2022-08-31 16:21:10 -0500

    added generation esports project and updated enchanted quill project -- still have to update technologies

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -32,33 +32,29 @@ function Projects() {
                         >
                             <figure className="col-10 col-md-4 col-lg-3 project project1">
                                 <div className="description">
-                                    <p className="project-title">Enchanted Quill</p>
-                                    <p>React/TypeScript/Express/MySQL ecommerce site under construction for Enchanted Quill Artwork, LLC.</p>
+                                    <p className="project-title">Generation Esports</p>
+                                    <p>Esports platform for tournament/streaming/team management.<br /><br /> Angular | TypeScript | NestJS | Node.js | Express | PHP | MySQL | MongoDB | Redis | TypeORM | gRPC | GraphQL | REST | AWS</p>
                                     <div className="link-buttons">
-                                        <a href="https://github.com/rflctvEQ/laughing-broccoli" target="_blank" className="github-link">GitHub Repo</a>
-                                        <a href="https://calm-oasis-15641.herokuapp.com/" target="_blank" className="project-link">Visit Site</a>
+                                        <a href="https://app.generationesports.com" target="_blank" className="project-link">Visit Site</a>
                                     </div>
                                 </div>
                             </figure>
                             <figure className="col-10 col-md-4 col-lg-3 project project2">
+                                <div className="description">
+                                    <p className="project-title">Enchanted Quill</p>
+                                    <p>React/TypeScript/Express/MySQL ecommerce site under construction for Enchanted Quill Artwork, LLC.</p>
+                                    <div className="link-buttons">
+                                        <a href="https://enchantedquillart.com" target="_blank" className="project-link">Visit Site</a>
+                                    </div>
+                                </div>
+                            </figure>
+                            <figure className="col-10 col-md-4 col-lg-3 project project3">
                                 <div className="description">
                                     <p className="project-title">This Game</p>
                                     <p>React/TypeScript interactive story with subpar writing.</p>
                                     <div className="link-buttons">
                                         <a href="https://github.com/rflctvEQ/this-game" target="_blank" className="github-link">GitHub Repo</a>
                                         <a href="https://stoic-kowalevski-619236.netlify.app/" target="_blank" className="project-link">Visit Site</a>
-                                    </div>
-                                </div>
-                            </figure>
-                            <figure className="col-10 col-md-4 col-lg-3 project project3">
-                                <div className="description">
-                                    <p className="project-title">Inspecto-Gadget</p>
-                                    <p>MVP of React/JS/Express/MySQL app building inspectors in Orchard Lake, MI use for tracking inspections.</p>
-                                    <p className='username'>User: Electrical</p>
-                                    <p className='password'>Password: pass12345</p>
-                                    <div className="link-buttons">
-                                        <a href="https://github.com/Laurenzel93/inspecto-gadget" target="_blank" className="github-link">GitHub Repo</a>
-                                        <a href="https://arcane-taiga-84005.herokuapp.com/" target="_blank" className="project-link">Visit Site</a>
                                     </div>
                                 </div>
                             </figure>
@@ -75,6 +71,18 @@ function Projects() {
                         >
                             <figure className="col-10 col-md-4 col-lg-3 project project4">
                                 <div className="description">
+                                    <p className="project-title">Inspecto-Gadget</p>
+                                    <p>MVP of React/JS/Express/MySQL app building inspectors in Orchard Lake, MI use for tracking inspections.</p>
+                                    <p className='username'>User: Electrical</p>
+                                    <p className='password'>Password: pass12345</p>
+                                    <div className="link-buttons">
+                                        <a href="https://github.com/Laurenzel93/inspecto-gadget" target="_blank" className="github-link">GitHub Repo</a>
+                                        <a href="https://arcane-taiga-84005.herokuapp.com/" target="_blank" className="project-link">Visit Site</a>
+                                    </div>
+                                </div>
+                            </figure>
+                            <figure className="col-10 col-md-4 col-lg-3 project project5">
+                                <div className="description">
                                     <p className="project-title">Group Mentality</p>
                                     <p>JS/Handlebars/Experss/MySQL app where you can post to a public forum and have direct interactions with mental health specialists.</p>
                                     <div className="link-buttons">
@@ -83,23 +91,13 @@ function Projects() {
                                     </div>
                                 </div>
                             </figure>
-                            <figure className="col-10 col-md-4 col-lg-3 project project5">
+                            <figure className="col-10 col-md-4 col-lg-3 project project6">
                                 <div className="description">
                                     <p className="project-title">Tech Blog</p>
                                     <p>JS/Handlebars/Experss/MySQL app for techies to blog about their latest projects.</p>
                                     <div className="link-buttons">
                                         <a href="https://github.com/rflctvEQ/tech-blog" target="_blank" className="github-link">GitHub Repo</a>
                                         <a href="https://afternoon-retreat-69044.herokuapp.com/" target="_blank" className="project-link">Visit Site</a>
-                                    </div>
-                                </div>
-                            </figure>
-                            <figure className="col-10 col-md-4 col-lg-3 project project6">
-                                <div className="description">
-                                    <p className="project-title">Employee Directory</p>
-                                    <p>React/JavaScript app that renders a searchable and sortable employee directory from the Random User API.</p>
-                                    <div className="link-buttons">
-                                        <a href="https://github.com/rflctvEQ/employee-directory" target="_blank" className="github-link">GitHub Repo</a>
-                                        <a href="https://rflctveq.github.io/employee-directory" target="_blank" className="project-link">Visit Site</a>
                                     </div>
                                 </div>
                             </figure>
